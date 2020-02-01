cfccea06c957ae4b92d5dc279555ea605d524a0b
mgramin <mgramin@gmail.com>
2020-02-01 22:26:53 +0300

    Create big_cities.sql

---
+++ sql/big_cities.sql
@@ -0,0 +1,20 @@
+/*
+{
+  "name": "big_cities",
+  "title": "Big cities",
+  "description": "Cities with more than one airport"
+}
+*/
+select a.airport_code as code
+     , a.airport_name
+     , a.city
+     , a.coordinates
+     , a.timezone
+  from bookings.airports a
+ where a.city in (select aa.city
+                    from bookings.airports aa
+                   group by aa.city
+                  having count(*) > 1)
+ order by
+       a.city
+     , a.airport_code
